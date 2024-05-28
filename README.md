3a7812c5545918aac7fb87402b8b454b40572d08
Jay Phillips <jayphillips23@my.whitworth.edu>
2024-05-28 13:46:11 -0700

    Game loop

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # Pacman-AI
 
-# Compiling and Running #
+## This Project ##
+The first part of this project follows a tutorial for SDL2 game development
+You can find it here: https://www.youtube.com/watch?v=44tO977slsU&t=418s
+The tutorial serves as a guide for developing general game structure, but is not specific to Pacman
+
+## Compiling and Running ##
 Everything has been configured to easily compile and run
 Type: "make" in the terminal to compile the project. I'm using mingw64.
 Type: "make run" to run the project.
 
-# A Note about SDL #
+## A Note about SDL ##
 This project uses the C++ SDL2 and SDL_image third party libraries. These libraries are open source.
 This project comes with the libraries, so you can download them here or use the links below
 
-# Downloading SDL #
+## Downloading SDL ##
 Download SDL: https://github.com/libsdl-org/SDL/releases/tag/release-2.30.3
 Download SDL_image: https://github.com/libsdl-org/SDL_image/releases
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <Game/Game.h>
 
 
@@ -6,23 +5,20 @@
 //       It's purpose is to test the project structure and verify a successful compilation and run
 //       The video tutorial uses Visual Studio, but this project has been configured for VSCode
 
+Game* game = nullptr;
+
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* win = SDL_CreateWindow("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
-    SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, 0);
+    game = new Game();
+    game->init("Pacman", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // White transparent background
-
-    SDL_Surface* tempSurface = IMG_Load("assets/mario.png");
-    SDL_Texture* playerTex = SDL_CreateTextureFromSurface(renderer, tempSurface);
-    SDL_FreeSurface(tempSurface);
-
-
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, playerTex, NULL, NULL);
-    SDL_RenderPresent(renderer);
-    SDL_Delay(3000); // Display the window for 3 seconds
+    while (game->running())
+    {
+        game->handleEvents();
+        game->update();
+        game->render();
+    }
+    game->clean();
 
     return 0;
 }
